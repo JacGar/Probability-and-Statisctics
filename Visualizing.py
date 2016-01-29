c300ded4357103d70c2dc2cfef4f960128697318
Jacinto Garcia <jacintogm84@gmail.com>
2016-01-29 19:34:41 +0100

    Saved pngs instead of creating new window

--- Visualizing.py
+++ Visualizing.py
@@ -6,14 +6,18 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 loansData.dropna(inplace=True)
 
 loansData.boxplot(column='Amount.Requested')
+plt.savefig("Box Requested.png")
 loansData.hist(column='Amount.Requested')
+plt.savefig("Hist Requested.png")
 
 plt.figure()
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.savefig("QQ Requested.png")
 plt.figure()
 loansData.boxplot(column='Amount.Funded.By.Investors')
+plt.savefig("Box Funded.png")
 loansData.hist(column='Amount.Funded.By.Investors')
-
+plt.savefig("Hist Funded.png")
 plt.figure()
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
-plt.show()
+plt.savefig("QQ Funded.png")
